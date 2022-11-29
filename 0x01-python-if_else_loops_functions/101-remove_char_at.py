634026cc68645f721dbcb6d8e2ee352ec53b88a4
GiftiMulugeta <gakmt7@gmail.com>
2022-11-29 09:22:32 -0800

    if loops functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        length = len(str)
-        if (n > length or n < 0):
-            print("{}".format(i), end="")
-        else:
-            if (i == str[n]):
-                continue
-            print("{}".format(i), end="")
+    if (n < 0):
+        return (str)
+    return (str[0:n] + str[n+1:])
