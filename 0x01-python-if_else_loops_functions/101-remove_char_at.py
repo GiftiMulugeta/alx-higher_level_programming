7494075e5d895381265b4ff3726d79b41ec7586e
GiftiMulugeta <gakmt7@gmail.com>
2022-11-29 08:50:04 -0800

    if loops functions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    for i in str:
+        length = len(str)
+        if (n > length or n < 0):
+            print("{}".format(i), end="")
+        else:
+            if (i == str[n]):
+                continue
+            print("{}".format(i), end="")
+    print()
