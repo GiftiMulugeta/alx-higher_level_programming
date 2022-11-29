b7196a5984f6726b168baad6ba6ead305afa308f
GiftiMulugeta <gakmt7@gmail.com>
2022-11-29 03:58:57 -0800

    if loops function

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if (number < 0):
-    print(f"{number} is negative")
-elif (number > 0):
-    print(f"{number} is positive")
-
-else:
-    print(f"{number} is zero")
+for i in range(97, 123):
+    print(chr(i), end="")
