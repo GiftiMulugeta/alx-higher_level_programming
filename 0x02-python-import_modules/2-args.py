b02c52ddf1ee2e9c891666e0831c194ae4346009
GiftiMulugeta <gakmt7@gmail.com>
2022-12-01 13:08:01 -0800

    imports and modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+    print("{}: {}".format(len(sys.argv) - 1, sys.argv[len(sys.argv) - 1]))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
