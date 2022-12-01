dfa7581b7e2c5904c225cf433b2dfb68ec939df4
GiftiMulugeta <gakmt7@gmail.com>
2022-12-01 13:09:47 -0800

    imports and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("{} argument:".format(len(sys.argv) - 1))
-    print("{}: {}".format(len(sys.argv) - 1, sys.argv[len(sys.argv) - 1]))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}: {}".format(len(sys.argv) - 1, sys.argv[len(sys.argv) - 1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
