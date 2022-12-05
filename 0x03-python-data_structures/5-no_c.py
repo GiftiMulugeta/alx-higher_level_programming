dd51c6bf384d5160d3b64cf8fbf21dfcb09c68eb
GiftiMulugeta <gakmt7@gmail.com>
2022-12-04 23:02:11 -0800

    datastructure

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    s = []
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            s.append(i)
+    print(s)
