23c931b30ec57c11d435d395aacca9a17c388eb6
GiftiMulugeta <gakmt7@gmail.com>
2022-12-05 08:43:28 -0800

    datastructure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    for i in my_string:
-        res = my_string.replace('c', '').replace('C', '')
-        return res
+    new = ""
+    for i in range(0, len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        else:
+            new = new + my_string[i]
+    return new
