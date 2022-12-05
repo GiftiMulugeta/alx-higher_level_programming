192819508e1b001faedab2a6b72f4b2a7e6004ae
GiftiMulugeta <gakmt7@gmail.com>
2022-12-05 14:52:16 -0800

    ds

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    #new = ""
-    #for i in range(len(my_string)):
-     #   if my_string[i] == 'c' or my_string[i] == 'C':
-    #        continue
-   # else:
-     #   new = new + my_string[i]
-    #return new
     copy = [x for x in my_string if x!= 'c' and x != 'C']
     return("".join(copy))
