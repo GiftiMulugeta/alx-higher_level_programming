36ac66453a19a96bab0982dac1df454b18ca5bf3
GiftiMulugeta <gakmt7@gmail.com>
2022-12-07 12:34:19 -0800

    more ds

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            x = matrix[i][j]
-            square = x * x
-            if j != (len(matrix[i])) - 1:
-                print(" ", end="")
-                return square
-        print("")
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
