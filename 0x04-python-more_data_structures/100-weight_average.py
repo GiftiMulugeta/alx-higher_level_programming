d8f466ee8c107126b30489e842108753beea9735
GiftiMulugeta <gakmt7@gmail.com>
2022-12-08 12:38:36 -0800

    more ds

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     mul = 0
     summ = 0
+    if len(my_list) == 0:
+        return 0
     for i in my_list:
         mul = mul + (i[0] * i[1])
         summ = summ + i[1]
