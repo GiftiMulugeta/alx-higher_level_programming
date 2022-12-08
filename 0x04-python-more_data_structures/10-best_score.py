efefd1a45766d5ccc9deaa272c3eb6a377f481c0
GiftiMulugeta <gakmt7@gmail.com>
2022-12-08 13:38:32 -0800

    more ds

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if len(a_dictionary) == 0:
+        return None
     keymax = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
     return keymax
