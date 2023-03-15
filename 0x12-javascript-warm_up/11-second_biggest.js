d953dfceef4bce31a88f4c6eb6cc5d8ded66e59e
GiftiMulugeta <gakmt7@gmail.com>
2023-03-15 12:35:18 +0000

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length <= 3) {
+//const args = process.argv;
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let i = 0;
-  let max = 0;
-  for (; i < args.length; i++) {
-    if (args[i + 2] < args[i + 3]) {
-      max = args[i + 3];
-      args[i + 3] = max;
-    }
-  } console.log(max);
-}
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+  }
