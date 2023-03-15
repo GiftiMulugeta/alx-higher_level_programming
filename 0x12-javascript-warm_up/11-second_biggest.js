88785abe47daa8cb54f48c7bcdee748b01476376
GiftiMulugeta <gakmt7@gmail.com>
2023-03-15 12:21:23 +0000

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,11 @@ if (args.length <= 3) {
   console.log(0);
 } else {
   let i = 0;
-  let max = args[i + 2];
+  let max = 0;
   for (; i < args.length; i++) {
-    if (args[i + 3] > args[i + 2]) {
+    if (args[i + 2] < args[i + 3]) {
       max = args[i + 3];
+      args[i + 3] = max;
     }
   } console.log(max);
 }
