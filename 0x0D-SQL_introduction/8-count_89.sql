e2e083b966701c02e80afad5dbcc1b1d2551c2c2
GiftiMulugeta <gakmt7@gmail.com>
2023-03-21 02:14:28 -0700

    sql_intor

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 --  a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT SUM(TABLE_ROWS) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'hbtn_0c_0';
+SELECT COUNT(*) FROM first_table WHERE id =89;
